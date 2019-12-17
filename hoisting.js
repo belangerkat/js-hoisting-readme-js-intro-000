454439e775e2aa71fdf6b12564d0f031988a482f
Katie Belanger <belangerkat@gmail.com>
2019-12-17 17:55:44 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -10,12 +10,11 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
-  function thisIsCrazy(){
+  var thisIsCrazy = function (){
     console.log("hey!!!")
-  }
 }
+
+thisIsCrazy();
 
 function sayMyName() {
   // fix the code in here:
